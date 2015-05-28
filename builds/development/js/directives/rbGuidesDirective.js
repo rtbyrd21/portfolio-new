24444b123feb8fd9ca9cca2b03c25d9e729bff6e
Rob Byrd <byrd.t.rob@gmail.com>
2015-05-28 00:16:31 -0400

    ok

--- builds/development/js/directives/rbGuidesDirective.js
+++
@@ -1,9 +0,0 @@
-
-
-angular.module('app').directive('rbGuides', function(){
-  return{
-    scope:{
-    },
-    templateUrl: 'js/directives/directiveTemplates/healingCenter.html',
-  }
-});
--- builds/development/js/directives/rbLocationsDirective.js
+++
@@ -1,9 +0,0 @@
-
-
-angular.module('app').directive('rbLocations', function(){
-  return{
-    scope:{
-    },
-    template: '<h1>Location Page</h1>'
-  }
-});
