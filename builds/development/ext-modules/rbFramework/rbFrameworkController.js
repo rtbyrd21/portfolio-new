e3071ce46cad6bfe6fecee55fc1615d2206fc17d
Rob Byrd <byrd.t.rob@gmail.com>
2015-05-30 23:31:50 -0400

    ok

--- builds/development/ext-modules/rbFramework/rbFrameworkController.js
+++ builds/development/ext-modules/rbFramework/rbFrameworkController.js
@@ -58,7 +58,7 @@ angular.module('rbFramework')
         checkWidth();
         broadcastMenuState();
         determineViewWidth();
-        setMenuTop();
+//        setMenuTop();
       });
     });
   
